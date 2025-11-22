025d79b3b9d91e6bb713c6d885824d064028a906
EduSantos <eduardofelipi@gmail.com>
2025-11-22 17:10:18 -0300

    core: initial setup

---
+++ src/main/kotlin/com/edufelip/meer/MeerApplication.kt
@@ -0,0 +1,11 @@
+package com.edufelip.meer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MeerApplication>(*args)
+}
