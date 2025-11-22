f0fa9f266f18f9d0640a3fe70e2171e02f214e11
EduSantos <eduardofelipi@gmail.com>
2025-11-22 17:10:18 -0300

    core: initial setup

---
+++ src/test/kotlin/com/edufelip/meer/MeerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.edufelip.meer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MeerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
