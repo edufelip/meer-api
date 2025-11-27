9d673eb2d9e9ecfba8fe5de12c5e55205550f2a6
EduSantos <eduardofelipi@gmail.com>
2025-11-27 20:19:38 -0300

    feat: removing whatsapp from social

--- springboot/src/main/java/com/edufelip/meer/core/store/Social.java
+++ springboot/src/main/java/com/edufelip/meer/core/store/Social.java
@@ -10,25 +10,20 @@ public class Social {
     private String instagram;
     @jakarta.persistence.Column(length = 2048)
     private String website;
-    @jakarta.persistence.Column(length = 2048)
-    private String whatsapp;
 
     public Social() {}
 
-    public Social(String facebook, String instagram, String website, String whatsapp) {
+    public Social(String facebook, String instagram, String website) {
         this.facebook = facebook;
         this.instagram = instagram;
         this.website = website;
-        this.whatsapp = whatsapp;
     }
 
     public String getFacebook() { return facebook; }
     public String getInstagram() { return instagram; }
     public String getWebsite() { return website; }
-    public String getWhatsapp() { return whatsapp; }
 
     public void setFacebook(String facebook) { this.facebook = facebook; }
     public void setInstagram(String instagram) { this.instagram = instagram; }
     public void setWebsite(String website) { this.website = website; }
-    public void setWhatsapp(String whatsapp) { this.whatsapp = whatsapp; }
 }
