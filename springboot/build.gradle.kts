1c9a3613590ee60e3ce1ea5cd8a78566d686544a
EduSantos <eduardofelipi@gmail.com>
2025-11-27 21:51:27 -0300

    feat: adding swagger

--- springboot/build.gradle.kts
+++ springboot/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
+    developmentOnly("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
     implementation("org.springframework.security:spring-security-crypto")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
