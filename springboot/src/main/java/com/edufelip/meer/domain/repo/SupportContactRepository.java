c563e9ba909b02b4e6c68b69af34f10aa571418c
EduSantos <eduardofelipi@gmail.com>
2025-12-01 09:05:19 -0300

    feat: implementing support messages

---
+++ springboot/src/main/java/com/edufelip/meer/domain/repo/SupportContactRepository.java
@@ -0,0 +1,6 @@
+package com.edufelip.meer.domain.repo;
+
+import com.edufelip.meer.core.support.SupportContact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupportContactRepository extends JpaRepository<SupportContact, Integer> {}
---
+++ springboot/src/main/java/com/edufelip/meer/dto/SupportContactRequest.java
@@ -0,0 +1,3 @@
+package com.edufelip.meer.dto;
+
+public record SupportContactRequest(String name, String email, String message) {}
